5b56d080e125e6f09ef9b0419a901eb4564b91f1
Sharon Dagan <sharon.dagan@gmail.com>
2020-04-08 18:13:21 +0300

    No time to commit.. My people need me!

--- openshift/lib/one-leg/terraform.tfvars
+++ openshift/lib/one-leg/terraform.tfvars
@@ -3,7 +3,12 @@ ssh_key        = "imac-key"
 
 cluster               = "peto"
 domain                = "rusty.ibmcloud"
-leg_1_vpc             = "peto-leg-1-rusty-ibmcloud"
+leg_1_vpc             = "${var.cluster}-leg-1"
 leg_1_vpc_region      = "eu-gb"
 leg_1_vpc_zone        = "eu-gb-1"
 leg_1_iaas_datacenter = "lon02"
+
+# internal
+
+installation_server_post_install_script = "xxx"
+network_server_post_install_script      = ""
--- openshift/lib/scripts/installation-server-post-install.sh
+++ openshift/lib/scripts/installation-server-post-install.sh
@@ -15,22 +15,35 @@ apt-get \
   --allow-change-held-packages -y \
   dist-upgrade
 
+# terraform
 cd /tmp
 curl -LO https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
 unzip /tmp/terraform*.zip -d /tmp
 mv /tmp/terraform /usr/local/bin
 
-curl -Lo /tmp/ibmcloud_terraform.zip https://github.com/IBM-Cloud/terraform-provider-ibm/releases/download/v1.2.5/linux_amd64.zip; unzip /tmp/ibmcloud_terraform.zip -d /tmp; mkdir -p ~/.terraform.d/plugins; mv /tmp/terraform-provider-ibm* ~/.terraform.d/plugins
-  - mkdir -p /usr/local/src; git clone https://github.com/ipxe/ipxe /usr/local/src/ipxe; cd /usr/local/src/ipxe/src; make
-  - curl -sL https://ibm.biz/idt-installer | bash; echo 'source /usr/local/ibmcloud/autocomplete/bash_autocomplete' >> /root/.bashrc
-  - cd /root; echo 'vpc-infrastructure dns cloud-object-storage kp tke vpn' | xargs -n 1 ibmcloud plugin install
-  - gem install --no-document bundle sinatra thin
-  - git clone https://github.com/j4zzcat/j4zzcat-ibmcloud /usr/local/src/j4zzcat-ibmcloud
-  - cd /tmp; curl -LO wget https://mirrors.mit.edu/ubuntu-cdimage/releases/19.10/release/ubuntu-19.10-server-amd64.iso
-  - mkdir -p /opt/openshift; cd /opt/openshift
-  - curl -Lo /tmp/openshift-install.tgz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux-4.3.9.tar.gz; tar -xzvf /tmp/openshift-install.tgz
-  - curl -Lo /tmp/openshift-client.tgz https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz; tar -xzvf /tmp/openshift-client.tgz
-  - ssh-keygen -t rsa -b 4096 -N '' -f /opt/openshift/rsa_id
+# ibmcloud terraform provider
+cd /tmp
+curl -LO https://github.com/IBM-Cloud/terraform-provider-ibm/releases/download/v1.2.5/linux_amd64.zip
+unzip /tmp/linux_amd64.zip -d /tmp
+mkdir -p ~/.terraform.d/plugins
+mv /tmp/terraform-provider-ibm* ~/.terraform.d/plugins
+
+# ibmcloud cli
+cd /root
+curl -sL https://ibm.biz/idt-installer | bash
+echo 'source /usr/local/ibmcloud/autocomplete/bash_autocomplete' >> /root/.bashrc
+echo 'vpc-infrastructure dns cloud-object-storage kp tke vpn' | xargs -n 1 ibmcloud plugin install
+
+# openshift
+cd /tmp
+curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-install-linux-4.3.9.tar.gz
+curl -LO https://mirror.openshift.com/pub/openshift-v4/clients/ocp/latest/openshift-client-linux.tar.gz
+mkdir -p /opt/openshift
+cd /opt/openshift
+tar -xzvf /tmp/openshift-install*.tgz
+tar -xzvf /tmp/openshift-client*.tgz
+
+# ssh-keygen -t rsa -b 4096 -N '' -f /opt/openshift/rsa_id
 
 
 
