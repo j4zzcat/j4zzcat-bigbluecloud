d0a71a07ef6896e85f018698eab1e5e466bcad14
Sharon Dagan <sharon.dagan@gmail.com>
2020-04-16 17:42:38 +0300

    Well, it's doing something.

--- openshift/modules/network_server/name_server_helper.rb
+++ openshift/modules/network_server/name_server_helper.rb
@@ -1,10 +1,26 @@
 require 'sinatra/base'
+require 'ipaddress'
 
 PORT = '7080'
 
 class NameServerHelper
   def run
+    WebApp.set :my_ip,     IPAddress::IPv4.new( %x[ hostname -I ].strip )
+    WebApp.set :my_subnet, IPAddress::IPv4.new( %x[ ip addr | grep $(hostname -I) | awk '{print $2}' ].strip ).network
     WebApp.set :my_domain, %x[ cat /etc/dnsmasq.conf | awk -F '=' '/^domain=/{print $2}' ].chomp
+    WebApp.set :my_reserved_ips, [
+      WebApp.settings.my_ip,
+      WebApp.settings.my_subnet.first,
+      IPAddress::IPv4.parse_u32( WebApp.settings.my_subnet.first.to_i + -1 ),
+      IPAddress::IPv4.parse_u32( WebApp.settings.my_subnet.first.to_i + 1 ),
+      IPAddress::IPv4.parse_u32( WebApp.settings.my_subnet.first.to_i + 2 ),
+      WebApp.settings.my_subnet.last ]
+
+    puts "Starting..."
+    puts "my_ip: #{WebApp.settings.my_ip.to_s}"
+    puts "my_subnet: #{WebApp.settings.my_subnet.to_s}"
+    puts "my_domain: #{WebApp.settings.my_domain}"
+    puts "my_reserved_ips: #{WebApp.settings.my_reserved_ips.map { |ip| ip.to_s } }"
 
     Rack::Server.start( {
       server: 'thin',
@@ -26,15 +42,28 @@ class NameServerHelper
     end
 
     post '/registar/:ip' do
-      ip       = params[ 'ip' ]
+      ip = nil
+      begin
+        ip = IPAddress::IPv4.new params[ 'ip' ]
+      rescue
+        return 400
+      end
+
+      # check for reserved ips
+      return 400 if settings.my_reserved_ips.include?( ip )
+
+      # check for duplicates
+      return 409 if %x[ cat /etc/hosts | grep --count -e "^#{ip.to_s}" ].chomp != '0'
+
+      # only allow ips in my own subnet
+      return 400 if !settings.my_subnet.include? ip
+
+      # validate hostname
       hostname = params[ 'hostname' ]
+      return 400 if /^[a-zA-Z0-9][a-zA-Z0-9\-]*$/.match( hostname ) == nil
+      return 400 if hostname.length > 32
 
-      return 400 if hostname.count( '.' ) > 0
-      return 400 if hostname.count( ' ' ) > 0
-      return 409 if %x[ cat /etc/hosts | grep --count -e "^#{ip}" ].chomp != '0'
-      # TODO test if given ip is in our subnet
-
-      %x[ echo "#{ip} #{hostname}.#{settings.my_domain}" >> /etc/hosts ]
+      %x[ echo "#{ip.to_s} #{hostname}.#{settings.my_domain}" >> /etc/hosts ]
       200
     end
   end
--- openshift/scripts/ubuntu_18/install_sinatra.sh
+++ openshift/scripts/ubuntu_18/install_sinatra.sh
@@ -1,4 +1,4 @@
 DEBIAN_FRONTEND=noninteractive apt-get install -y \
   ruby2.5-dev gcc g++ make
 
-gem install --no-document bundle sinatra thin
+gem install --no-document bundle sinatra thin ipaddress
