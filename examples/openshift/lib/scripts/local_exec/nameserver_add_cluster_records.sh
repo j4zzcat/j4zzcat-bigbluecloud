8043a21039ab0cab1f8abac5cea166aff5d30e7c
Sharon Dagan <sharon.dagan@gmail.com>
2020-04-18 05:02:24 +0300

    simplicfication

--- examples/openshift/lib/scripts/local_exec/nameserver_add_cluster_records.sh
+++ examples/openshift/lib/scripts/local_exec/nameserver_add_cluster_records.sh
@@ -8,16 +8,18 @@ MASTER_1_PIP=${7}
 MASTER_2_PIP=${8}
 MASTER_3_PIP=${9}
 
+FQDN=${CLUSTER_NAME}.${DOMAIN_NAME}
+
 echo -e "\
-  host-record=api.${CLUSTER_NAME}.${DOMAIN_NAME}.,${HAPROXY_MASTERS_PIP}\n\
-  host-record=api-int.${CLUSTER_NAME}.${DOMAIN_NAME}.,${HAPROXY_MASTERS_PIP}\n\
-  host-record=*.apps.${CLUSTER_NAME}.${DOMAIN_NAME}.,${HAPROXY_WORKERS_PIP}\n\
-  host-record=etcd-0.${CLUSTER_NAME}.${DOMAIN_NAME}.,${MASTER_1_PIP}\n\
-  host-record=etcd-1.${CLUSTER_NAME}.${DOMAIN_NAME}.,${MASTER_2_PIP}\n\
-  host-record=etcd-2.${CLUSTER_NAME}.${DOMAIN_NAME}.,${MASTER_3_PIP}\n\
-  srv-host=_etcd-server-ssl._tcp.${CLUSTER_NAME}.${DOMAIN_NAME}.,etcd-0.${CLUSTER_NAME}.${DOMAIN_NAME},2380,0,10\n\
-  srv-host=_etcd-server-ssl._tcp.${CLUSTER_NAME}.${DOMAIN_NAME}.,etcd-1.${CLUSTER_NAME}.${DOMAIN_NAME},2380,0,10\n\
-  srv-host=_etcd-server-ssl._tcp.${CLUSTER_NAME}.${DOMAIN_NAME}.,etcd-2.${CLUSTER_NAME}.${DOMAIN_NAME},2380,0,10\n\
+  host-record=api.${FQDN}.,${HAPROXY_MASTERS_PIP}\n\
+  host-record=api-int.${FQDN}.,${HAPROXY_MASTERS_PIP}\n\
+  host-record=*.apps.${FQDN}.,${HAPROXY_WORKERS_PIP}\n\
+  host-record=etcd-0.${FQDN}.,${MASTER_1_PIP}\n\
+  host-record=etcd-1.${FQDN}.,${MASTER_2_PIP}\n\
+  host-record=etcd-2.${FQDN}.,${MASTER_3_PIP}\n\
+  srv-host=_etcd-server-ssl._tcp.${FQDN}.,etcd-0.${FQDN},2380,0,10\n\
+  srv-host=_etcd-server-ssl._tcp.${FQDN}.,etcd-1.${FQDN},2380,0,10\n\
+  srv-host=_etcd-server-ssl._tcp.${FQDN}.,etcd-2.${FQDN},2380,0,10\n\
 " | ssh \
   -oStrictHostKeyChecking=no \
   -i ${KEY_FILE} \
