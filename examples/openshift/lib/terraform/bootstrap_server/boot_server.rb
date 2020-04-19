43f456f9df6df507229c18ae831e9be1a4da71d3
Sharon Dagan <sharon.dagan@gmail.com>
2020-04-19 04:28:43 +0300

    Reset error count between rows. herpderp

--- examples/openshift/lib/terraform/bootstrap_server/boot_server.rb
+++ examples/openshift/lib/terraform/bootstrap_server/boot_server.rb
@@ -33,19 +33,19 @@ class BootstrapServer
         apt install -y ipxe
         sed --in-place -e 's/GRUB_DEFAULT=0/GRUB_DEFAULT=ipxe/' /etc/default/grub
         sed --in-place -e 's/--class network {/--class network --id ipxe {/' /etc/grub.d/20_ipxe
-        sed --in-place -e 's|linux16.*|linux16 $IPXEPATH dhcp \\\\&\\\\& chain http://#{settings.my_ip}:#{settings.my_port}/boot?type=#{type}|' /etc/grub.d/20_ipxe
+        sed --in-place -e 's|linux16.*|linux16 $IPXEPATH dhcp \\\\&\\\\& chain http://#{settings.my_ip}:#{settings.my_port}/boot/#{type}|' /etc/grub.d/20_ipxe
         update-grub
         # reboot
       EOT
     end
 
-    get '/boot/master' do
+    get '/boot/:type' do
       type = params[ 'type' ]
       return <<~EOT
         #!ipxe
         dhcp
         route
-        kernel http://#{settings.my_ip}/openshift/rhcos/rhcos-4.3.8-x86_64-installer-kernel-x86_64 coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=http://#{settings.my_ip}/openshift/rhcos/metal.x86_64.raw.gz coreos.inst.ignition_url=http://#{settings.my_ip}/openshift/install/coppermine.dollar/master_config.ign ip=dhcp nameserver=10.242.0.6 rd.neednet=1
+        kernel http://#{settings.my_ip}/openshift/rhcos/rhcos-4.3.8-x86_64-installer-kernel-x86_64 coreos.inst=yes coreos.inst.install_dev=sda coreos.inst.image_url=http://#{settings.my_ip}/openshift/rhcos/metal.x86_64.raw.gz coreos.inst.ignition_url=http://#{settings.my_ip}/openshift/install/coppermine.dollar/#{type}_config.ign ip=dhcp nameserver=10.242.0.6 rd.neednet=1
         initrd http://#{settings.my_ip}/openshift/rhcos/rhcos-4.3.8-x86_64-installer-initramfs.x86_64.img
         boot
       EOT
