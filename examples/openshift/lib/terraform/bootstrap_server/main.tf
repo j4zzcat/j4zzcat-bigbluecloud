c946cdee41073f757fa27b4c3389368494e2f830
Sharon Dagan <sharon.dagan@gmail.com>
2020-04-22 10:30:05 +0300

    fixed errors in the previous commit

--- examples/openshift/lib/terraform/bootstrap_server/main.tf
+++ examples/openshift/lib/terraform/bootstrap_server/main.tf
@@ -19,13 +19,47 @@ module "bootstrap_server" {
     var.security_groups[ "allow_inbound_openshift_bootstrap" ]
   ]
 
-  user_data = join( "\n", [
-    "#cloud-config", "runcmd:",
-    "-", file( "${path.module}/../../scripts/ubuntu_18/upgrade_os.sh" ),
-    "-", file( "${path.module}/../../scripts/ubuntu_18/install_basics.sh" ),
-    "-", file( "${path.module}/../../scripts/ubuntu_18/install_sinatra.sh" ),
-    "-", templatefile( "${path.module}/../../scripts/ubuntu_18/config_resolve.sh", { domain_name = var.domain_name, nameserver_ip = var.nameserver_ip } ),
-    "-", file( "${path.module}/../../scripts/openshift/install_client.sh" ),
-    "-", file( "${path.module}/../../scripts/openshift/generate_config.sh" ),
-    "power_state:\nmode: reboot\ntimeout: 1\ncondition: True" ] )
+  user_data = <<-EOT
+    #cloud-init
+    runcmd:
+      - mkdir -p /usr/local/src/scripts
+  EOT
 }
+
+resource "null_resource" "bootstrap_server_config" {
+  provisioner "file" {
+    source      = "${path.module.bootstrap_server}/../../scripts/ubuntu_18"
+    destination = "/usr/local/src/scripts"
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = file( var.keys[ 0 ] )
+      host        = module.bootstrap_server.public_ip
+    }
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "bash -c 'for script in upgrade_os install_basics install_ipxe install_sinatra; do bash /usr/local/src/${script}.sh; done; reboot'"
+    ]
+
+    connection {
+      type        = "ssh"
+      user        = "root"
+      private_key = file( var.admin_key )
+      host        = module.bootstrap_server.public_ip
+    }
+  }
+}
+
+#   user_data = join( "\n", [
+#     "#cloud-config", "runcmd:",
+#     "-", file( "${path.module}/../../scripts/ubuntu_18/upgrade_os.sh" ),
+#     "-", file( "${path.module}/../../scripts/ubuntu_18/install_basics.sh" ),
+#     "-", file( "${path.module}/../../scripts/ubuntu_18/install_sinatra.sh" ),
+#     "-", templatefile( "${path.module}/../../scripts/ubuntu_18/config_resolve.sh", { domain_name = var.domain_name, nameserver_ip = var.nameserver_ip } ),
+#     "-", file( "${path.module}/../../scripts/openshift/install_client.sh" ),
+#     "-", file( "${path.module}/../../scripts/openshift/generate_config.sh" ),
+#     "power_state:\nmode: reboot\ntimeout: 1\ncondition: True" ] )
+# }
