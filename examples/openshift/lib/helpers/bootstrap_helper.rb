a2a71040ac1453ad6128e5493b27e30b7f738a14
Sharon Dagan <sharon.dagan@gmail.com>
2020-05-02 01:48:45 +0300

    maybe tomorrow...

--- examples/openshift/lib/helpers/bootstrap_helper.rb
+++ examples/openshift/lib/helpers/bootstrap_helper.rb
@@ -1,21 +1,15 @@
 require 'sinatra/base'
 require 'ipaddress'
 
-HELPER_IP      = IPAddress::IPv4.new( %x[ hostname -I ].strip )
-HELPER_PORT    = '7080'
-HELPER_SUBNET  = IPAddress::IPv4.new( %x[ ip addr | grep $(hostname -I) | awk '{print $2}' ].strip ).network
-HELPER_NETMASK = HELPER_SUBNET.netmask
-HELPER_GATEWAY = %x[ ip route show default | awk '/#{HELPER_IP.to_s}/{print $3}' ].chomp
-HELPER_DNS     = %x[ systemd-resolve --status | tail | awk -F ':' '/DNS Servers/{print $2}'].strip
-HELPER_DOMAIN  = %x[ systemd-resolve --status | tail | awk -F ':' '/DNS Domain/{print $2}'].strip
+HELPER_IP         = IPAddress::IPv4.new( %x[ hostname -I ].strip )
+HELPER_PORT       = '7080'
+HELPER_DNS        = %x[ systemd-resolve --status | tail | awk -F ':' '/DNS Servers/{print $2}'].strip
+HELPER_DOMAIN     = %x[ systemd-resolve --status | tail | awk -F ':' '/DNS Domain/{print $2}'].strip
+HELPER_STATE_DIR  = '.'
+HELPER_REGISTAR   = "#{HELPER_STATE_DIR}/#{File.basename( $0, File.extname( $0 ) )}.registar"
+HELPER_PUBLIC_DIR = '/var/sinatra/www'
 
-HELPER_STATE_DIR = "."
-HELPER_REGISTAR  = "#{HELPER_STATE_DIR}/#{File.basename( $0, File.extname( $0 ) )}.registar"
-
-# OPENSHIFT_CLUSTER_NAME = %x[ ls -l /opt/openshift/install | awk '/#{HELPER_DOMAIN}/{print $9}' | awk -F '.' '{print $1}' ]
-OPENSHIFT_CLUSTER_NAME = 'rover'
-
-BAD_REQUEST = 400
+OPENSHIFT_WWW     = "http://#{HELPER_IP}:#{HELPER_PORT}/openshift"
 
 class BootstrapServer
   def run
@@ -36,7 +30,7 @@ class BootstrapServer
   class WebApp < Sinatra::Base
     configure do
       enable :logging
-      set :public_folder, '/var/sinatra/www'
+      set :public_folder, HELPER_PUBLIC_DIR
     end
 
     get '/netmask_of' do
@@ -87,18 +81,17 @@ class BootstrapServer
       net_gateway         = params[ 'net_gateway' ]
       openshift_node_type = params[ 'openshift_node_type' ]
 
-      record = "#{instance_id} #{net_ip} #{net_netmask} #{net_gateway} #{openshift_node_type}"
+      # make sure client ip resolves to a name
+      net_fqhn = %x[ nslookup #{net_ip} | head -n 1 | awk -F '=' '{print $2}' ].strip[ 0..-2 ]
+
+      record = "#{instance_id} #{net_ip} #{net_netmask} #{net_gateway} #{net_fqhn} #{openshift_node_type}"
       %x[ sed --in-place -e '/^#{instance_id} .*/d' #{HELPER_REGISTAR} ]
       %x[ echo "#{record}" >> #{HELPER_REGISTAR} ]
     end
 
     get '/boot/:instance_id' do
       instance_id = params[ 'instance_id' ]
-      pk, net_ip, net_netmask, net_gateway, openshift_node_type = %x[ cat #{HELPER_REGISTAR} | grep -e '^#{instance_id}' ].chomp.split
-
-      # client_type = params[ 'type' ]
-      # client_ip   = request.ip
-      # client_fqhn = %x[ nslookup #{client_ip} | head -n 1 | awk -F '=' '{print $2}' ].strip[ 0..-2 ]
+      pk, net_ip, net_netmask, net_gateway, net_fqhn, openshift_node_type = %x[ cat #{HELPER_REGISTAR} | grep -e '^#{instance_id}' ].chomp.split
 
       configure_ip = <<~EOT
         ifopen net0
@@ -108,16 +101,16 @@ class BootstrapServer
       EOT
 
       kernel = <<~EOT
-        kernel http://#{HELPER_IP}/openshift/rhcos/rhcos-4.3.8-x86_64-installer-kernel-x86_64 \
+        kernel #{OPENSHIFT_WWW}/rhcos/rhcos-4.3.8-x86_64-installer-kernel-x86_64 \
           coreos.inst=yes \
           coreos.inst.install_dev=sda \
-          coreos.inst.image_url=http://#{HELPER_IP}/openshift/rhcos/metal.x86_64.raw.gz \
-          coreos.inst.ignition_url=http://#{HELPER_IP}/openshift/install/#{OPENSHIFT_CLUSTER_NAME}.#{HELPER_DOMAIN}/#{client_type.to_s}_config.ign \
-          ip=#{client_ip}::#{HELPER_GATEWAY}:#{HELPER_NETMASK}:#{client_fqhn} nameserver=#{HELPER_DNS}
+          coreos.inst.image_url=#{OPENSHIFT_WWW}/rhcos/metal.x86_64.raw.gz \
+          coreos.inst.ignition_url=#{OPENSHIFT_WWW}/install/#{openshift_node_type}_config.ign \
+          ip=#{net_ip}::#{net_gateway}:#{net_netmask}:#{net_fqhn} nameserver=#{HELPER_DNS}
       EOT
 
       initrd = <<~EOT
-        http://#{HELPER_IP}/openshift/rhcos/rhcos-4.3.8-x86_64-installer-initramfs.x86_64.img
+        #{OPENSHIFT_WWW}/rhcos/rhcos-4.3.8-x86_64-installer-initramfs.x86_64.img
       EOT
 
       return <<~EOT
