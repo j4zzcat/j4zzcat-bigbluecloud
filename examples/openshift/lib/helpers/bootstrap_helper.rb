c0826e500b1ebaeac9640cfa66acb504eebb7614
Sharon Dagan <sharon.dagan@gmail.com>
2020-05-03 17:06:50 +0300

    This changes nothing, don't look

--- examples/openshift/lib/helpers/bootstrap_helper.rb
+++ examples/openshift/lib/helpers/bootstrap_helper.rb
@@ -42,36 +42,33 @@ class BootstrapServer
     # get network interface name
     # ip addr | grep -B2 'inet 10.215.122.26' | head -n 1 | awk -F ':' '{print $2}' | sed 's/ //'
 
-    get '/probe' do
-      client_ip = request.ip
-
-      probe = <<~EOT
-        instance_id=$(cloud-init query instance_id)
-        net_hostname=$(hostname)
-        net_address=$(ip addr | grep -e 'inet ' | grep #{client_ip} | awk '{print $2}')
-        net_netmask=$(curl -X GET --data "net_address=${net_address}" http://#{HELPER_IP}:#{HELPER_PORT}/calculate_netmask)
-        net_ip=$(echo ${net_address} | awk -F '/' '{print $1}')
-        net_gateway=$(ip route show default | awk '{print $3}')
-        echo -e "instance_id=${instance_id}\nnet_hostname=${net_hostname}\nnet_address=${net_address}\nnet_netmask=${net_netmask}\nnet_ip=${net_ip}\nnet_gateway=${net_gateway}"
-      EOT
-
-      return probe
-    end
-
     get '/prepare/:openshift_node_type' do
       client_ip           = request.ip
       openshift_node_type = params[ 'openshift_node_type' ]
 
       probe = <<~EOT
         instance_id=$(cloud-init query instance_id)
+        blk_slash=$(lsblk | awk '/part \/$/{print $1}' | tr -cd '[:print:]' | tr -cd '[a-zA-Z]')
+        net_interface=$(ip link show | grep -e '^[0-9][0-9]*:' | grep -iv loopback | awk '{print $2}' | sed -e 's/://')
+
         net_hostname=$(hostname)
         net_address=$(ip addr | grep -e 'inet ' | grep #{client_ip} | awk '{print $2}')
-        net_netmask=$(curl -X GET \
-                        --data "net_address=${net_address}" \
-                        http://#{HELPER_IP}:#{HELPER_PORT}/calculate_netmask)
+        net_netmask=$(curl -X GET --data "net_address=${net_address}" http://#{HELPER_IP}:#{HELPER_PORT}/calculate_netmask)
         net_ip=$(echo ${net_address} | awk -F '/' '{print $1}')
         net_gateway=$(ip route show default | awk '{print $3}')
+
+        echo -e "Registering host instance_id=${instance_id} \
+          \nblk_slash=${blk_slash}         \
+          \nnet_interface=${net_interface} \
+          \nnet_hostname=${net_hostname}   \
+          \nnet_address=${net_address}     \
+          \nnet_netmask=${net_netmask}     \
+          \nnet_ip=${net_ip}               \
+          \nnet_gateway=${net_gateway} "
+
         curl -X POST \
+          --data "blk_slash=${blk_slash}" \
+          --data "net_interface=${net_interface}" \
           --data "net_hostname=${net_hostname}" \
           --data "net_ip=${net_ip}" \
           --data "net_netmask=${net_netmask}" \
@@ -98,6 +95,8 @@ class BootstrapServer
 
     post '/register/:instance_id' do
       instance_id         = params[ 'instance_id' ]
+      blk_slash           = params[ 'blk_slash' ]
+      net_interface       = params[ 'net_interface' ]
       net_hostname        = params[ 'net_hostname' ]
       net_fqhn            = "#{net_hostname}.#{OPENSHIFT_CLUSTER_NAME}.#{HELPER_DOMAIN}"
       net_ip              = params[ 'net_ip' ]
@@ -105,14 +104,14 @@ class BootstrapServer
       net_gateway         = params[ 'net_gateway' ]
       openshift_node_type = params[ 'openshift_node_type' ]
 
-      record = "#{instance_id} #{net_fqhn} #{net_ip} #{net_netmask} #{net_gateway} #{openshift_node_type}"
+      record = "#{instance_id} #{blk_slash} #{net_interface} #{net_fqhn} #{net_ip} #{net_netmask} #{net_gateway} #{openshift_node_type}"
       %x[ sed --in-place -e '/^#{instance_id} .*/d' #{HELPER_REGISTAR} ] if File.exist? HELPER_REGISTAR
       %x[ echo "#{record}" >> #{HELPER_REGISTAR} ]
     end
 
     get '/boot/:instance_id' do
       instance_id = params[ 'instance_id' ]
-      pk, net_fqhn, net_ip, net_netmask, net_gateway, openshift_node_type = %x[ cat #{HELPER_REGISTAR} | grep -e '^#{instance_id}' ].chomp.split
+      pk, blk_slash, net_interface, net_fqhn, net_ip, net_netmask, net_gateway, openshift_node_type = %x[ cat #{HELPER_REGISTAR} | grep -e '^#{instance_id}' ].chomp.split
 
       configure_ip = <<~EOT
         ifopen net0
@@ -126,11 +125,11 @@ class BootstrapServer
         kernel #{OPENSHIFT_WWW}/rhcos/rhcos-4.3.8-x86_64-installer-kernel-x86_64 \
           initrd=#{OPENSHIFT_WWW}/rhcos/rhcos-4.3.8-x86_64-installer-initramfs.x86_64.img \
           coreos.inst=yes \
-          coreos.inst.install_dev=sda \
+          coreos.inst.install_dev=#{blk_slash} \
           coreos.inst.image_url=#{OPENSHIFT_WWW}/rhcos/rhcos-4.3.8-x86_64-metal.x86_64.raw.gz \
           coreos.inst.ignition_url=#{OPENSHIFT_WWW}/install/#{openshift_node_type}.ign \
           rd.neednet=1 console=tty0 console=ttyS0 \
-          ip=#{net_ip}::#{net_gateway}:#{net_netmask}:#{net_fqhn}:eth0:none nameserver=#{HELPER_DNS}
+          ip=#{net_ip}::#{net_gateway}:#{net_netmask}:#{net_fqhn}:#{net_interface}:none nameserver=#{HELPER_DNS}
       EOT
 
       initrd = <<~EOT
